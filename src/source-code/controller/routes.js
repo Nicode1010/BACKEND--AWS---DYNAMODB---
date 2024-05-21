bf46573b03b589a8105eefc71b3933a13a2b7765
Nicode1010 <nicolasrios@unicauca.edu.co>
2024-05-21 11:42:56 -0500

    Clean code

--- src/source-code/controller/routes.js
+++ src/source-code/controller/routes.js
@@ -13,22 +13,22 @@ const {
 
 // Importing the functions from the S3 SDK
 const {
-  uploadS3File,
-  ListS3Files,
-  getS3File,
-  deleteS3File,
+  uploadS3File, //Importa la funcion para cargar un archivo en Amazon S3
+  ListS3Files, //Importa la funcion para listar archivos en Amaazon S3
+  getS3File,// Importa la función para obtener un archivo de Amazon S3
+  deleteS3File,// Importa la función para eliminar un archivo de Amazon S3
 } = require("../aws/s3");
 
-const api = express.Router();
+const api = express.Router(); // Crea una instancia de Router de Express para manejar las rutas de la API
 
-// Endpoint para crear una orden (POST)
+// Endpoint para crear una orden (POST) crear
 api.post("/create-order", async (request, response) => {
   try {
-    const orderData = request.body; // Utiliza los datos del cuerpo de la solicitud
+    const orderData = request.body; // Obtiene los datos del cuerpo de la solicitud
 
     // Guardar la orden en DynamoDB
     await putDynamoDBItem(orderData);
-
+//Respuesta del backend al "cliente"
     response
       .status(StatusCodes.OK)
       .json({ msg: "Tu orden ha sido creada", orderData });
@@ -71,41 +71,5 @@ api.delete("/delete-order/:id_pedido", async (request, response) => {
     response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Internal Server Error" });
   }
 });
-/*
-api.post("/path2", upload.single("file"), async (request, response) => {
-  try {
-    console.info("BODY", request.file);
 
-    const fileInfo = request.file;
-    console.info("FILE INFO", fileInfo);
-
-    const { originalname, buffer, mimetype } = fileInfo;
-
-    // Upload a file to S3
-    await uploadS3File({ key: originalname, buffer, mimetype });
-
-    // List all files from S3
-    const s3Files = await ListS3Files();
-    console.info("S3 Files", s3Files);
-
-    // Get the file from S3
-    const s3File = await getS3File(originalname);
-    console.info(`S3 File With Name ${originalname}`, s3File);
-
-    // Delete the file from S3
-    await deleteS3File(originalname);
-
-    response.writeHead(StatusCodes.OK, {
-      "content-disposition": `attachment; filename=${originalname}`,
-      "Content-Type": mimetype,
-    });
-    response.end(buffer);
-  } catch (error) {
-    console.error("Error", error);
-    response
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ msg: "Internal Server Error" });
-  }
-});
-*/
 module.exports = api;
